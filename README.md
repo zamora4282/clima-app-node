882847e9e75142a5c455596c7b93e1ea74556db9
David <zamora4282@gmail.com>
2020-05-03 11:51:54 +0200

    Realizamos el primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicación del Clima 
+
+Recuerden ejecutar ```npm install``` para las librerias
+
+### Ejemplo:
+```
+node app -d "Barcelona"
+```
---
+++ lugar/lugar.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+
+    const encodeUrl = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+        timeout: 1000,
+        headers: { 'x-rapidapi-key': '9eaecba43fmshc3392fe81cb3f8bp193392jsn98219fde98aa' }
+    });
+
+    const resp = await instance.get()
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${ dir }`)
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+}
+
+module.exports = {
+    getLugarLatLng
+}
